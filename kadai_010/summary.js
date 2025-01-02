e9cdbe9dd6252b7df488fcd9ce58f9e35d0b7709
mikeru-design-git <design.mikeru@gmail.com>
2025-01-02 16:29:31 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,22 @@
+$(function() {
+  // 色を変える
+  $('#change-color').on('click', () => {
+    $('#target').css('color', 'red');
+  });
+
+  // 文字内容変化
+  $('#change-text').on('click', () => {
+    $('#target').text('Hello!');
+  });
+
+  // フェードアウト
+  $('#fade-out').on('click', () => {
+    $('#target').fadeOut();
+  });
+
+  // フェードイン
+  $('#fade-in').on('click', () => {
+    $('#target').fadeIn();
+  });
+
+});
